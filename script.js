ea1dda5bfcf3476e92ef3d4e87c08c5e644286d0
Azamat Yagygerov <azamat1696@gmail.com>
2020-12-29 23:17:43 +0200

    ilk commit

---
+++ script.js
@@ -0,0 +1,110 @@
+const slidePage=document.querySelector(".slidepage");
+const firstNextBtn=document.querySelector(".nextBtn");
+const prevBtnSec=document.querySelector(".prev-1");
+const nextBtnSec=document.querySelector(".next-1");
+
+const prevBtnThrid=document.querySelector(".prev-2");
+const nextBtnThrid=document.querySelector(".next-2");
+
+const prevBtnFourth=document.querySelector(".prev-3");
+const submitBtn=document.querySelector(".submit");
+
+const progressText=document.querySelectorAll(".step p");
+const progressCheck=document.querySelectorAll(".step .check");
+const bullet=document.querySelectorAll(".step .bullet");
+
+let max=4;
+let current=1;
+
+
+
+firstNextBtn.addEventListener("click",function(){
+
+   slidePage.style.marginLeft ="-25%";
+ 
+   bullet[current - 1].classList.add("active");
+   progressCheck[current - 1].classList.add("active");
+   progressText[current - 1].classList.add("active");
+   current += 1;
+});
+prevBtnSec.addEventListener("click",function(){
+
+    slidePage.style.marginLeft ="0%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+ 
+ });
+
+
+ nextBtnSec.addEventListener("click",function(){
+
+    slidePage.style.marginLeft ="-50%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+
+    current += 1;
+ });
+/********************** */
+
+
+ prevBtnThrid.addEventListener("click",function(){
+
+    slidePage.style.marginLeft ="-25%";
+
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+ 
+ });
+ 
+
+ nextBtnThrid.addEventListener("click",function(){
+
+    slidePage.style.marginLeft ="-75%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+
+    current += 1;
+ 
+ });
+
+
+ prevBtnFourth.addEventListener("click",function(){
+
+    slidePage.style.marginLeft ="-50%";
+
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+ 
+ });
+
+/**submit btn */
+
+ /*submitBtn.addEventListener("click",function(){
+
+    slidePage.style.marginLeft ="-100%";
+
+    bullet[current - 1].classList.add("active");
+
+    progressCheck[current - 1].classList.add("active");
+   progressText[current - 1].classList.add("active");
+
+    current += 1;
+ 
+    setTimeout(function formYaz(){
+      var fName=document.getElementById("firstName").value;
+      var lName=document.getElementById("lastName").value;
+        alert("First name:"+fName +" "
+               + "Last name : " +lName  );
+        location.reload();
+    }, 800);
+
+
+ });*/
